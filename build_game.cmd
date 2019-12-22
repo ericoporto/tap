1797962c6e9824927d6671b11ec1ad33c96990e4
ericoporto <ericoporto2008@gmail.com>
2019-12-22 00:27:26 -0300

    adds automation for appveyor

---
+++ build_game.cmd
@@ -0,0 +1,14 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set AGSEDITOR="C:\Program Files (x86)\Adventure Game Studio 3.5.0\AGSEditor.exe"
+set AGSGAMEPROJECT=%SYSTEMDRIVE%\projects\ags_test_game
+
+if [%~1]==[] goto :NOPARAM
+set AGSGAMEPROJECT="%~1"
+
+:NOPARAM
+
+call %AGSEDITOR% /compile %AGSGAMEPROJECT%\ags_test_game\Game.agf
+
+endlocal
