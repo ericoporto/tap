eef79fd1527e2ee64bf439c295ba7831bece81e2
ericoporto <ericoporto2008@gmail.com>
2019-12-22 01:17:35 -0300

    update for appveyor also run game

---
+++ run_game.cmd
@@ -0,0 +1,15 @@
+@echo on
+setlocal enabledelayedexpansion
+
+set AGSGAMEPROJECT=%SYSTEMDRIVE%\projects\ags-test-game
+set SAVEGAMEDIR=%USERPROFILE%\Saved Games\
+
+if [%~1]==[] goto :NOPARAM
+set AGSGAMEPROJECT="%~1"
+
+:NOPARAM
+
+call %AGSGAMEPROJECT%\ags_test_game\Compiled\Windows\ags_test_game.exe
+dir %SAVEGAMEDIR%
+
+endlocal
