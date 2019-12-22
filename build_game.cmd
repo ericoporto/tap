c0756f584b8691cdc562b868ab8e8464356bc0a1
ericoporto <ericoporto2008@gmail.com>
2019-12-22 01:00:20 -0300

    remove unused prints to console

--- build_game.cmd
+++ build_game.cmd
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 setlocal enabledelayedexpansion
 
 set AGSEDITOR="C:\Program Files (x86)\Adventure Game Studio 3.5.0\AGSEditor.exe"
@@ -8,9 +8,6 @@ if [%~1]==[] goto :NOPARAM
 set AGSGAMEPROJECT="%~1"
 
 :NOPARAM
-
-dir "C:\Program Files (x86)\"
-dir %AGSGAMEPROJECT%
 
 call %AGSEDITOR% /compile %AGSGAMEPROJECT%\ags_test_game\Game.agf
 
